484cf8200f4b6724c1b4189839dd8423e1cdf350
jaksik <connorjaksik@gmail.com>
2025-05-15 19:11:50 -0500

    accidentaly edited on main

---
+++ pages/news.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/ModeToggle"
+
+export default function Home() {
+  return (
+    <div>
+      {/* Your page content here */}
+      <ModeToggle/>
+      
+      <h1>News</h1>
+    </div>
+  )
+}
